7fee5be3623e42805f6adf8f657742803a81fb9c
Viswa220522 <nossamviswateja@gmail.com>
2024-11-21 07:58:56 +0000

    Draw_cat_plot

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight']/(df['height']/100)**2 >25).astype(int)
 
 # 3
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int)
+df['gluc'] = (df['gluc'] > 1).astype(int)
 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
+    df_cat = =pd.melt(
+    df,
+    id_vars=['cardio'], 
+    value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'],
+    var_name='Feature',
+    value_name='Value'
+)
 
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(['Feature', 'Value', 'cardio']).size().reset_index(name='Count')
     
 
     # 7
-
-
-
+    df_cat.rename(columns={'cardio' : 'Cardio'}, inplace=True)
+    
+    catplot=sns.catplot(
+    data=df_cat,
+    x='Value', y='Count', hue='Cardio', col='Feature',
+    kind='bar',
+    height=4, aspect=0.8
+    )
+    
     # 8
-    fig = None
+    fig = catplot.figure
 
 
     # 9
